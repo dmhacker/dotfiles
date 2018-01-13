fe8e524204bec446f1b6f756f7e28221defe4d0e
David Hacker <dmhacker@yahoo.com>
2018-01-13 12:03:00 -0800

    Updated .bashrc to tmp include PATH modification

--- rc/.bashrc
+++ rc/.bashrc
@@ -1,9 +1,18 @@
 
+# Add local binaries to PATH
+# This is duplicated from .profile (TODO: fix hacky solution)
+if [ -d "$HOME/.local/bin" ]; then
+	PATH="$HOME/.local/bin:$PATH"
+fi
+
 # Set terminal color format
 export TERM="xterm-256color"
 export LANG="en_US.UTF-8" 
 
+# Configure powerline for the user
 powerline-daemon -q
 POWERLINE_BASH_CONTINUATION=1
 POWERLINE_BASH_SELECT=1
-. /home/dmhacker/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+if [ -f ~/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh ]; then
+  source ~/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+fi
--- setup.sh
+++ setup.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-shopt -s dotglob # Enable bash to read hidden files
+# Install powerline for the current user 
+pip install --user git+git://github.com/powerline/powerline
+if [ -e ~/.profiledankfiles ]; then 
+  echo "Mod file found: ~/.local/bin already added to .profile"  
+else
+  echo "Modifying .profile to include ~/.local/bin ..."
+  echo -e "\n# Add local binaries to PATH" >> ~/.profile
+  echo -e "if [ -d \"\$HOME/.local/bin\" ]; then" >> ~/.profile
+  echo -e "\tPATH=\"\$HOME/.local/bin:\$PATH\"" >> ~/.profile 
+  echo -e "fi" >> ~/.profile
+  touch ~/.profiledankfiles
+fi
 
+# Install powerline fonts for the current user 
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Install powerline globally (default = off)
+# sudo pip install powerline-status
+# sudo apt-get install fonts-powerline
+
+# Create symlinks from rc/ to ~/
+shopt -s dotglob # Enable bash to read hidden files
 for filepath in rc/*
 do
   abspath="$(realpath $filepath)" # The file that the symlink points to
