8c5c67ee438b4ac675fd3303ce51bad48109992a
David Hacker <dmhacker.cs@gmail.com>
2018-05-20 16:08:57 -0700

    Modify theme colors slightly and make polybar RAM display smaller

--- .config/powerline-shell/themes/nord.py
+++ .config/powerline-shell/themes/nord.py
@@ -13,26 +13,26 @@ class Color(DefaultColor):
     HOME_SPECIAL_DISPLAY = True
     HOME_BG = 4
     HOME_FG = 7
-    PATH_BG = 8  # dark grey
-    PATH_FG = 7  # light grey
-    CWD_FG = 15  # white
+    PATH_BG = 8  
+    PATH_FG = 7 
+    CWD_FG = 15
     SEPARATOR_FG = 12
 
     READONLY_BG = 1
     READONLY_FG = 15
 
-    REPO_CLEAN_BG = 2   # green
-    REPO_CLEAN_FG = 0   # black
-    REPO_DIRTY_BG = 1   # red
-    REPO_DIRTY_FG = 15  # white
+    REPO_CLEAN_BG = 2   
+    REPO_CLEAN_FG = 0  
+    REPO_DIRTY_BG = 11
+    REPO_DIRTY_FG = 0
 
     JOBS_FG = 14
     JOBS_BG = 8
 
     CMD_PASSED_BG = 8
     CMD_PASSED_FG = 15
-    CMD_FAILED_BG = 11
-    CMD_FAILED_FG = 0
+    CMD_FAILED_BG = 1
+    CMD_FAILED_FG = 15
 
     SVN_CHANGES_BG = REPO_DIRTY_BG
     SVN_CHANGES_FG = REPO_DIRTY_FG
