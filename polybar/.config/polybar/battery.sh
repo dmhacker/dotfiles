0bd32c7d70f036fe5f418cdf5f82127c0b97c836
David Hacker <dmhacker@protonmail.com>
2018-12-30 17:31:05 -0800

    ACPI driver fixed - remove old battery script

--- polybar/.config/polybar/battery.sh
+++
@@ -1,6 +0,0 @@
-if [ -f ~/.config/polybar/.battery_error ]; then
-  echo "  "
-else
-  sh ~/.config/polybar/battery_safeguard.sh &
-  python3 ~/.config/polybar/battery.py
-fi
--- polybar/.config/polybar/battery_safeguard.sh
+++
@@ -1,6 +0,0 @@
-sleep 3
-pkill -9 -f ~/.config/polybar/battery.py
-
-if [ $? -eq 0 ]; then
-  touch ~/.config/polybar/.battery_error
-fi
