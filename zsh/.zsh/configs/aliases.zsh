8b28132c074ac270fafcb8e1f52a2ab119a4adff
David Hacker <dmhacker@protonmail.com>
2019-01-17 16:27:36 -0800

    Add sbsign alias

--- zsh/.zsh/configs/aliases.zsh
+++ zsh/.zsh/configs/aliases.zsh
@@ -1,10 +1,14 @@
 alias vimconfig="vim ~/.vimrc"
 alias vimupdate="vim +PluginInstall +qall"
 alias viminstall="git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vimupdate"
+
 alias zshconfig="vim ~/.zshrc"
 alias zshplugins="vim ~/.zsh_plugins.txt"
 alias zshupdate="antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh"
+
 alias venvrun="source venv/bin/activate"
+
+alias kernelsign="sbsign --key ~/.secboot/mok/MOK.key --cert ~/.secboot/mok/MOK.crt --output"
 
 alias optirun="vblank_mode=0 optirun"
 alias primusrun="vblank_mode=0 primusrun"
