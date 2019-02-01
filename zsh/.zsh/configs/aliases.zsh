e3b86a6d29996ad3ea354ab8752a7fb1bd9b7e25
David Hacker <dmhacker@protonmail.com>
2019-01-31 19:52:21 -0800

    Add aliases for Surface kernel signing

--- zsh/.zsh/configs/aliases.zsh
+++ zsh/.zsh/configs/aliases.zsh
@@ -8,6 +8,9 @@ alias zshupdate="antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh"
 
 alias venvrun="source venv/bin/activate"
 
+alias surfacesign="sudo sbsign --key ~/.secboot/mok/MOK.key --cert ~/.secboot/mok/MOK.crt --output /boot/vmlinuz-linux-surface /boot/vmlinuz-linux-surface" 
+alias surfaceverify="sbverify --cert ~/.secboot/mok/MOK.crt /boot/vmlinuz-linux-surface" 
+
 alias optirun="vblank_mode=0 optirun"
 alias primusrun="vblank_mode=0 primusrun"
 
