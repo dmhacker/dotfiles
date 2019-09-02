a6cdcf27317a7c54ef1bfa6128ffb0ab17721550
David Hacker <dmhacker@protonmail.com>
2019-09-02 16:23:04 -0400

    Add docker commands to kill containers

--- zsh/.zsh/configs/aliases.zsh
+++ zsh/.zsh/configs/aliases.zsh
@@ -14,6 +14,9 @@ alias surfaceverify="sbverify --cert ~/.secboot/mok/MOK.crt /boot/vmlinuz-linux-
 alias optirun="vblank_mode=0 optirun"
 alias primusrun="vblank_mode=0 primusrun"
 
+alias dkkillall="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)"
+alias dksysprune="docker system prune"
+
 alias ls="ls --color=auto"
 alias la="ls -A"
 alias ll="ls -l"
