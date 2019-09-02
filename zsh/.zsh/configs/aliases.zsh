3deb71d944d2c1054860b0be2839e7f1204a2e9f
David Hacker <dmhacker@protonmail.com>
2019-09-02 16:25:32 -0400

    Simplify docker commands even more

--- zsh/.zsh/configs/aliases.zsh
+++ zsh/.zsh/configs/aliases.zsh
@@ -14,8 +14,8 @@ alias surfaceverify="sbverify --cert ~/.secboot/mok/MOK.crt /boot/vmlinuz-linux-
 alias optirun="vblank_mode=0 optirun"
 alias primusrun="vblank_mode=0 primusrun"
 
-alias dkrclean="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)"
-alias dkrprune="docker system prune"
+alias dkclean="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)"
+alias dkprune="docker system prune"
 
 alias ls="ls --color=auto"
 alias la="ls -A"
