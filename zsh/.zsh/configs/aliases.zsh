5691b983baad56e67f55fdb32d766c3e74ae18f9
David Hacker <dmhacker@protonmail.com>
2019-09-03 10:37:14 -0400

    Remove dkclean command

--- zsh/.zsh/configs/aliases.zsh
+++ zsh/.zsh/configs/aliases.zsh
@@ -14,7 +14,6 @@ alias surfaceverify="sbverify --cert ~/.secboot/mok/MOK.crt /boot/vmlinuz-linux-
 alias optirun="vblank_mode=0 optirun"
 alias primusrun="vblank_mode=0 primusrun"
 
-alias dkclean="docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)"
 alias dkprune="docker system prune"
 
 alias ls="ls --color=auto"
